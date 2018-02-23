8b0b2b4a09d630b94f392dc0e72328f7c83756e3
jerabaul29 <jean.rblt@gmail.com>
2018-02-23 18:32:56 +0100

    written CDV class

--- DevelopmentCode/PowerControl/client_device.cpp
+++ DevelopmentCode/PowerControl/client_device.cpp
@@ -1,24 +1,110 @@
-#include <client_device.h>
+/*
 
-CDV::CDV(int pin_feedback, int pin_control, int cycles_sleep) :
-pin_feedback(pin_feedback),
-pin_control(pin_control),
-cycles_sleep(cycles_sleep)
+Copyright (c) 2015 Jean Rabault jean.rblt@gmail.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+#include "client_device.h"
+
+CDV::CDV(int pin_feedback, int pin_control, int cycles_sleep, const float * const battery_voltage) :
+  pin_feedback(pin_feedback),
+  pin_control(pin_control),
+  cycles_sleep(cycles_sleep),
+  battery_voltage(battery_voltage),
+  device_awake(false)  // off by default
 {
-  device_awake = false;
-
-  perform_max_cycles_check = false;
-  perform_voltage_check = false;
 }
 
-CDV::CDV(int pin_feedback, int pin_control, int cycles_sleep, float (* get_voltage_volts)(void)) :
-pin_feedback(pin_feedback),
-pin_control(pin_control),
-cycles_sleep(cycles_sleep),
-get_voltage_volts(get_voltage_volts)
-{
+
+// update status: different if asleep or awake
+void CDV::update_status(void){
+  if (device_awake){
+    CDV::update_awake();
+  }
+  else{
+    CDV::update_asleep();
+  }
+}
+
+// update awake: just need to check if wants more current
+void CDV::update_awake(void){
+  if (digitalRead(pin_feedback)){
+    // nothing to do!
+  }
+  else{
+    CDV::switch_off();
+  }
+}
+
+// update asleep: check the counter
+void CDV::update_asleep(void){
+
+  // if time to wake up...
+  if (0 == cycles_counter){  // ------------------------------------------------
+
+    // if enough battery
+    if (*battery_voltage > THRESHOLD_VOLTAGE){ // --------------
+      // power
+      CDV::switch_on();
+
+      // give the time to boot
+      // NOTE: if slow bootloader, this may not be enough?
+      // NOTE: if extend the delay, be careful: the watchdog will fire after 8
+      // seconds
+      delay(6000);
+
+      // if wants to be awake
+      if (digitalRead(pin_feedback)){
+        // already on
+      }
+      // if does not want to be awake
+      else{
+        // switch off
+        CDV::switch_off();
+      }
+    }
+
+    // if not enough battery
+    else{  // --------------------------------------------------
+      CDV::switch_off();
+    }
+  }
+
+  // if not time to wake up, sleep
+  else{  // --------------------------------------------------------------------
+    cycles_counter--;
+  }
+}
+
+void CDV::switch_on(void){
+  device_awake = true;
+
+  pinMode(pin_control, OUTPUT);
+  digitalWrite(pin_control, HIGH);
+}
+
+void CDV::switch_off(void){
   device_awake = false;
 
-  perform_max_cycles_check = false;
-  perform_voltage_check = true;
+  pinMode(pin_control, INPUT);
+
+  cycles_counter = cycles_sleep;
 }
--- DevelopmentCode/PowerControl/client_device.h
+++ DevelopmentCode/PowerControl/client_device.h
@@ -8,32 +8,54 @@
  *
  * This also includes some checks, such as:
  * - checking if the battery is enough when performing a new start up
- * - allowing only a maximum number of cycles during which the device is awake in a row
+ * TODO - allowing only a maximum number of cycles during which the device is awake in a row
+ *   before considering it a hang
+ *
+ * by Jean RABAULT <jean.rblt@gmail.com>
+
+ Copyright (c) 2015 Jean Rabault jean.rblt@gmail.com
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of
+ this software and associated documentation files (the "Software"), to deal in
+ the Software without restriction, including without limitation the rights to
+ use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ of the Software, and to permit persons to whom the Software is furnished to do
+ so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
  *
  */
+
+ #if ARDUINO >= 100
+   #include "Arduino.h"
+ #else
+   #include "WProgram.h"
+ #endif
 
 #ifndef CLIENT_DEVICE
 #define CLIENT_DEVICE
 #define CLIENT_DEVICE_VERSION 1.1
 
-#if ARDUINO >= 100
-  #include "Arduino.h"
-#else
-  #include "WProgram.h"
+#ifndef THRESHOLD_VOLTAGE
+#define THRESHOLD_VOLTAGE 2.8
 #endif
 
 class CDV{
 
 public:
-  CDV(int pin_feedback, int pin_control, int cycles_sleep);
-
-  CDV(int pin_feedback, int pin_control, int cycles_sleep, float (* get_voltage_volts)(void));
-
-  void set_cycles_sleep(int cycles_sleep);
+  CDV(int pin_feedback, int pin_control, int cycles_sleep, const float * const battery_voltage);
 
   void update_status(void);
-
-  void print_status(HardwareSerial *port);
 
 private:
 
@@ -42,23 +64,18 @@ private:
   //////////////////////////////////////////////////////////////////////////////
 
   // pins definition -----------------------------------------------------------
-  int pin_feedback;  // feedback from the device
-  int pin_control;  // control of the device
+  const int pin_feedback;  // feedback from the device
+  const int pin_control;  // control of the device
 
   // status of the device ------------------------------------------------------
   bool device_awake;
 
   // control of number of sleeps -----------------------------------------------
   int cycles_sleep;  // how many cycles should sleep before wake up
-  int max_cycles_awake;  // max number of cycles can be awake
   int cycles_counter;  // counter for either cycles sleep left or cycles awake elapsed
 
-  // options for the class -----------------------------------------------------
-  bool perform_voltage_check;  // if the voltage should be checked at wake up
-  bool perform_max_cycles_check;  // if should check for max number of cycles awake
-
   // check battery levels ------------------------------------------------------
-  float (* get_voltage_volts)(void);
+  const float * const battery_voltage;
   float voltage_volts;
   bool enough_volts;
 
@@ -70,11 +87,10 @@ private:
 
   void switch_off(void);
 
-  bool check_voltage(void);
+  void update_awake(void);
 
-  void switch_if_on(void);
+  void update_asleep(void);
 
-  void switch_if_off(void);
 
 };
 
