2f43be6a1903025a9bf8334c1e7462214b6f5054
jerabaul29 <jean.rblt@gmail.com>
2018-03-01 00:03:29 +0100

    remains to implement: VN100 and RaspberryPi interaction

--- DevelopmentCode/ArduinoMega/src/ArduinoMega.ino
+++ DevelopmentCode/ArduinoMega/src/ArduinoMega.ino
@@ -9,29 +9,12 @@
 // TODO: could use struct(char *, str_length) instead of plain char *. I do not want to use strings
 // as dynamically allocated, ok on computers, bad on mC.
 
-// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-// PRELIMINARY
-// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-// include libraries
-// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-#include <SPI.h>
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_LSM9DS0.h>
-#include <stdlib.h>
-
 #include "parameters.h"
 #include "GPS_controller.h"
 #include "EEPROM_interaction.h"
 #include "SDManager.h"
 #include "board_manager.h"
 #include "Iridium_manager.h"
-
-// global definitions; do not initialize, just define to make them global
 
 // board manager
 BoardManager board_manager{};
@@ -47,16 +30,9 @@ IridiumManager iridium_manager{&SERIAL_IRIDIUM, &gps_controller, &board_manager,
 
 
 // TODO: class for the VN100
-// TODO: class for interaction with Iridium
 // TODO: class for interaction with RaspberryPi
 // TODO: not urgent: class for the LSM9DS0
 // TODO: fix all names: name cpp file and header should be the same as class they contain
-
-// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-// SETUP LOOP
-// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 void setup(){
 
@@ -75,51 +51,17 @@ void setup(){
   // make Iridium ready
   iridium_manager.start();
   wdt_reset();
-}
 
-// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-// MAIN LOOP
-// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  // VN100
+
+  // raspberry Pi
+}
 
 void loop(){
 
   wdt_reset();
 
-  
-  /*
-  // when done logging: close logging file
-  dataFile.close();
+  // decide which step in the process at: board manager?
 
-  wdt_reset();
-
-  // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  // VITAL INFORMATION IRIDIUM
-  // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-  // send some diagnostic strings by Iridum, including detailed GPS position.
-  // done here as doing it at this point one most likely has a GPS fix and full
-  // Iridium capacitors
-  #if USE_IRIDIUM
-    send_receive_Iridium_vital_information();
-  #endif
-
-  // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  // RASPBERRY PI INTERACTION
-  // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-  #if SERIAL_RPI
-    raspberry_pi_interaction();
-  #endif
-
-  // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  // EVERYTHING IS DONE: REBOOT
-  // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-  // if it is time to sleep, this will be caught by the decide_if_wakeup function
-  wdt_enable(WDTO_15MS);
-  while(1){
-  }
-  */
+  // act in consequence.
 }
